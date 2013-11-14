271b3b0ed33f68a5d76721d6195308b6df9935f3
Michael Bottjer <michaelbottjer@hotmail.com>
2013-11-14 21:53:54 +0000

    more colours possible

--- public/colour.js
+++ public/colour.js
@@ -1,7 +1,19 @@
-$(document).mousemove(function(e){
-    var $width = ($(document).width())/255;
-    var $height = ($(document).height())/255;
-    var $pageX = parseInt(e.pageX / $width,10);
-    var $pageY = parseInt(e.pageY / $height,10);
-    $("body").css("background-color", "rgb("+$pageX+","+$pageY+","+$pageX+")");
-}); 
+var $win = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidthandHeight = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+$win.resize(getWidthandHeight).mousemove(function(event) {
+
+    rgb = [
+        Math.floor(event.pageX/w * 255),
+        Math.floor(event.pageY/h * 255),
+        100
+    ];
+
+    $(document.body).css('background','rgb('+rgb.join(',')+')');
+
+}).resize();
